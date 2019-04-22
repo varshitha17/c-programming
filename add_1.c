4a98be94f3bab2394cb47cef76ceabc30d632108
sneha222 <49440759+sneha222@users.noreply.github.com>
2019-04-22 09:27:29 +0530

    Create add_1.c

---
+++ add_1.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int input()
+{
+    int a;
+scanf("%d",&a);
+return a;
+}
+int compute(int a);
+{
+a=a+1;
+return a;
+}
+int output(int x);
+{
+printf("%d",&a);
+return;
+}
+int main()
+{
+
+
+    
+
+
+     
+     
+
+
